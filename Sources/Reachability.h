aed036bf44f6296b7a511aed3f7019418531cb60
Bojan Dimovski <bojand@gmail.com>
2016-10-26 22:43:23 +0200

    Rename the source directory to Sources.

--- Sources/Reachability.h
+++ Sources/Reachability.h
@@ -13,7 +13,3 @@ FOUNDATION_EXPORT double ReachabilityVersionNumber;
 
 //! Project version string for Reachability.
 FOUNDATION_EXPORT const unsigned char ReachabilityVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Reachability/PublicHeader.h>
-
-
