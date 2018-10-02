1deba98b3939103c638c6ec9fc93c660196cd404
Ashley Mills <ashleymills@mac.com>
2018-10-02 13:14:03 +0100

    Update for Swift 4.2

--- ReachabilityAppleTVSample/AppDelegate.swift
+++ ReachabilityAppleTVSample/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
 }
