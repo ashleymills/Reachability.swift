465f972cb97b3ccdae4b8444ccc52f6e79fc3e34
Sergei Shatunov <sergei@fubo.tv>
2019-12-05 17:51:06 -0500

    Swift PR Old Versions Support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Reachability",
+    platforms: [
+         .macOS(.v10_10), .iOS(.v8), .tvOS(.v9)
+    ],
     products: [
         .library(
             name: "Reachability",
